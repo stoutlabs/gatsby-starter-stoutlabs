720bd094d413c626a906a14e63cff17253351b01
Daniel Stout <daniel@stoutlabs.com>
2020-05-31 06:46:23 -0400

    updated deps to current. fixed helmet import for new version (in SEO component)

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,6 +1,6 @@
 import path from "path";
 import React from "react";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 import PropTypes from "prop-types";
 import config from "../config";
 
